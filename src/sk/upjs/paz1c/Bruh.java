cf1541a0794fd2405e641b4cb76953e8e856a00a
roman <roman@158.197.232.11>
2022-09-28 18:11:28 +0200

    Zmena od Romana.

---
+++ src/sk/upjs/paz1c/Bruh.java
@@ -0,0 +1,9 @@
+package sk.upjs.paz1c;
+
+public class Bruh {
+
+	public static void main(String[] args) {
+		System.out.println("bruh");
+	}
+
+}
--- src/sk/upjs/paz1c/HelloTrieda.java
+++ src/sk/upjs/paz1c/HelloTrieda.java
@@ -1,11 +1,13 @@
 package sk.upjs.paz1c;
 
 public class HelloTrieda {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("Zdravim! :)");
-		
+
+		System.out.println("Zdravim! :(");
+		System.out.println("Kde je tvoja bezpečnosť?!");
+		System.out.println(":-P");
+
 	}
 
 }
