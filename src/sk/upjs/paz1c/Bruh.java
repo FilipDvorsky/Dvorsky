cfd73b35219733b60489edb59af811d9f9e14624
Filip <Filip@158.197.55.246>
2022-10-05 16:30:55 +0200

    Funkcionalita 1 ale pokrac

--- src/sk/upjs/paz1c/Bruh.java
+++ src/sk/upjs/paz1c/Bruh.java
@@ -5,6 +5,7 @@ public class Bruh {
 	public static void main(String[] args) {
 		System.out.println("bruh");
 		System.out.println("Feature 1");
+		System.out.println("Featura 1 ale pokracovanie");
 	}
 
 }
